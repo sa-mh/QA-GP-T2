05b562e2ab8f1f4bef531088ec849ceaf7029d4f
SherazShahid017 <SShahid2@qa.com>
2020-11-27 15:31:29 +0000

    Subent for each loop added (need to extract ID)

--- Infra/Terraform/subnets/main.tf
+++ Infra/Terraform/subnets/main.tf
@@ -1,37 +1,45 @@
 resource "aws_subnet" "taskVPCSubnet" {
   vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone = "eu-west-1a"
+
+  iterator = cidr
+  for_each = var.cidrblock
+  cidr_block              = cidr.value
+
+  iterator = azone
+  for_each = var.az
+  availability_zone = azone.value
 
   tags = {
-    Name = "Task-Pub-Sub"
+    Name = "FP - Subnet"
     "kubernetes.io/cluster/project_cluster" = "shared"
   }
-}
-resource "aws_subnet" "rdsSubnet2" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.3.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone = "eu-west-1b"
 
-  tags = {
-    Name = "RDS-Pub-Sub-2"
-    "kubernetes.io/cluster/project_cluster" = "shared"
-  }
-}
-resource "aws_subnet" "rdsSubnet3" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.4.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone = "eu-west-1c"
+// }
+// resource "aws_subnet" "rdsSubnet2" {
+//   vpc_id                  = var.vpc_id
+//   cidr_block              = "10.0.3.0/24"
+//   map_public_ip_on_launch = "true"
+//   availability_zone = "eu-west-1b"
 
-  tags = {
-    Name = "RDS-Pub-Sub-3"
-    "kubernetes.io/cluster/project_cluster" = "shared"
-  }
+//   tags = {
+//     Name = "RDS-Pub-Sub-2"
+//     "kubernetes.io/cluster/project_cluster" = "shared"
+//   }
+// }
+// resource "aws_subnet" "rdsSubnet3" {
+//   vpc_id                  = var.vpc_id
+//   cidr_block              = "10.0.4.0/24"
+//   map_public_ip_on_launch = "true"
+//   availability_zone = "eu-west-1c"
+
+//   tags = {
+//     Name = "RDS-Pub-Sub-3"
+//     "kubernetes.io/cluster/project_cluster" = "shared"
+//   }
   
 }
+
 resource "aws_db_subnet_group" "rds_groups" {
   name = "rds_groups"
   subnet_ids = [aws_subnet.taskVPCSubnet.id, aws_subnet.rdsSubnet2.id, aws_subnet.rdsSubnet3.id]
--- Infra/Terraform/subnets/variable.tf
+++ Infra/Terraform/subnets/variable.tf
@@ -1,3 +1,21 @@
 variable "vpc_id" {
   description = "vpc id to be overriden"
 }
+
+variable "cidrblock" {
+  type = map
+  default = {
+    cidr1 = "10.0.1.0/24"
+    cidr2 = "10.0.3.0/24"
+    cidr3 = "10.0.4.0/24"
+  }
+}
+
+variable "az" {
+  type = map
+  default = {
+    zone1 = "eu-west-1a"
+    zone1 = "eu-west-1b"
+    zone1 = "eu-west-1c"
+  }
+}
