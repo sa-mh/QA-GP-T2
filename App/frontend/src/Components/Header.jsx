d266c0546f5fcd0a9cc5928a6d7c3c2b7358c56a
Lewis Thomson <lewisfalconer@outlook.com>
2020-12-01 17:35:16 +0000

    Formatting header navbar -LT

--- App/frontend/src/Components/Header.jsx
+++ App/frontend/src/Components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import logo from '../Reasources/logo.svg'
-import { Link, Router } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <>
+            <span>
+            <Link to="/"><img className="navbar-logo" src="https://qa-community.co.uk/static/qa_fill_primary.svg" alt="QA Logo" width="64" height="64"/></Link>
+            </span>
             <div className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/about">About</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/about">About</Link>
             </div>
         </>
         )
