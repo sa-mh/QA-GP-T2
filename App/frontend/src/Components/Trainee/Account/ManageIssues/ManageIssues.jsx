9aaa69379155cb08833cd81442cf22f3d1f3d8cc
Ramgithj <RJagajith@academytrainee.com>
2020-12-02 16:58:52 +0000

    Created trainee and trainer pages with states

---
+++ App/frontend/src/Components/Trainee/Account/ManageIssues/ManageIssues.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const ManageIssues = () => {
+
+    const [data, setData] = useState("");
+
+
+    // making a get request which pulls the issues from the database - NOT SURE IF THIS WORKS. please Fix!
+        axios.get("http://localhost:8080/?username=")
+        .then(response => {
+            console.log(response.data);
+            setData(response.data);
+        });
+
+
+    return (
+
+        <div>
+
+        </div>
+    );
+}
+export default ManageIssues;
