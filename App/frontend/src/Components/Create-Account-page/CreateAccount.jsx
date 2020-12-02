d9d09ac73c48d2667365fb28b41f9f0dafa14bdb
Lewis Thomson <lewisfalconer@outlook.com>
2020-12-02 17:06:23 +0000

    Added axiom request for login. Who knows if this works right now, but it'll be fun when we find out. -LT

--- App/frontend/src/Components/Create-Account-page/CreateAccount.jsx
+++ App/frontend/src/Components/Create-Account-page/CreateAccount.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 const CreateAccount = () => {
 
-    let DATABASE_URL = ""
     // setting the states
     const [firstname, setFirstname] = useState("");
     const [lastname, setLastname] = useState("");
@@ -47,7 +46,7 @@ const CreateAccount = () => {
 
     const post_createAccount = (e) => {
         e.preventDefault();
-        axios.post({DATABASE_URL}+"/create/trainee", {
+        axios.post("/create/trainee", {
             firstName: firstname,
             lastName: lastname,
             qaEmail: qaEmail,
@@ -57,7 +56,9 @@ const CreateAccount = () => {
         ).then(response =>{
             console.log(response);
             return  <Redirect  to="../Login" />;
-        }).catch(error => console.log(error));
+        }).catch(error => {
+            console.log(error.data)
+        });
     }
 
     //If password1 and password2 is exactly the same, then setPasswordTheSame as true - If it is false, we need to show an error on the page, if it is true, we can send this to the database.
@@ -78,8 +79,8 @@ const CreateAccount = () => {
                         <input className="signupInput" type="text" id="last-name" onChange={(e)=>updateLastName(e)} placeholder="Enter your last name" required></input> <br></br>
                         <input className="signupInput" type="text" id="qa_email" onChange={(e)=>updateEmail(e)} placeholder="Enter your QA Email address" required></input> <br></br>
                         <input className="signupInput" type="text" id="username" onChange={(e)=>updateUsername(e)} placeholder="Enter your username" required></input> <br></br>
-                        <input className="signupInput" type="password" id="password" onChange={(e)=>updatePassword1(e)} placeholder="Enter your password" required></input> <br></br>
-                        <input className="signupInput" type="password" id="password" onChange={(e)=>updatePassword2(e)} placeholder="Enter your password" required></input> <br></br>
+                        <input className="signupInput" type="password" id="password1" onChange={(e)=>updatePassword1(e)} placeholder="Enter your password" required></input> <br></br>
+                        <input className="signupInput" type="password" id="password2" onChange={(e)=>updatePassword2(e)} placeholder="Enter your password" required></input> <br></br>
                         <button className="btn btn-primary" id="signupButton" type="submit" onClick={(e) => post_createAccount(e)}>Create an account</button>
                     </form>
                 </div>
--- App/frontend/src/Components/Login-page/LoginPage.jsx
+++ App/frontend/src/Components/Login-page/LoginPage.jsx
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, {useState, Redirect} from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 // import { Switch } from 'react-router-dom'
 // import CreateAccount from '../Create-Account-page/CreateAccount'
 // import { BrowserRouter as Router, Route, } from 'react-router-dom';
 
 function LoginPage() {
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [memberType, setMemeberType] = useState("trainee");
+    
+    const updateUsername = (e) => {
+        e.preventDefault(); 
+        setUsername(e.target.value);
+    }
+
+    const updatePassword = (e) => {
+        e.preventDefault(); 
+        setPassword(e.target.value);
+    }
+
+    const updateMemberType = (e) => {
+        setMemeberType(e.target.value);
+    }
+
+    const get_Login = (e) => {
+        e.preventDefault();
+        axios.get("/get/" + memberType + "/" + username)
+        .then(response =>{
+            console.log(response);
+            if(response.data.password === password){
+              return  <Redirect  to="../{memberType}"/>;  
+            }
+            else{
+                //Show a login failed message
+            }
+        }).catch(error => {
+            console.log(error.data)
+        });
+    }
+
+
     return (
         <>
             <div className="loginDiv">
                 <h1 className="loginHeading">Login</h1>
                 <div>
                     <form className="ml-3" id="loginForm">
-                        <input className="loginInput" type="text" id="username" placeholder="Enter your username" required></input> <br></br>
-                        <input className="loginInput" type="password" id="password" placeholder="Enter your password" required></input> <br></br>
-                        <button className="btn btn-primary" id="loginButton" type="submit">Login</button> 
+                        <input className="loginInput" type="text" id="username" placeholder="Enter your username" onChange={(e)=>updateUsername(e)} required></input> <br></br>
+                        <input className="loginInput" type="password" id="password" placeholder="Enter your password" onChange={(e)=>updatePassword(e)} required></input> <br></br>
+                        <select onChange={(e) => updateMemberType(e)}>
+                            <option value="trainer">Trainer</option>
+                            <option value="trainee" selected>Trainee</option>
+                        </select>
+                        <button className="btn btn-primary" id="loginButton" type="submit" onClick={get_Login}>Login</button> 
 
                         <div>
                             <Link to="/createAccount">
