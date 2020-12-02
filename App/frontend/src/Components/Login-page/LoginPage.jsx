f1096b959729c8ec3093e3eb47d92fb46fdbba70
Ramgithj <RJagajith@academytrainee.com>
2020-12-02 16:59:24 +0000

    Created trainee and trainer pages with states

--- App/frontend/src/Components/Login-page/LoginPage.jsx
+++ App/frontend/src/Components/Login-page/LoginPage.jsx
@@ -13,8 +13,9 @@ function LoginPage() {
                     <form className="ml-3" id="loginForm">
                         <input className="loginInput" type="text" id="username" placeholder="Enter your username" required></input> <br></br>
                         <input className="loginInput" type="password" id="password" placeholder="Enter your password" required></input> <br></br>
+                        <Link to="/postIssue">
                         <button className="btn btn-primary" id="loginButton" type="submit">Login</button> 
-
+                        </Link>
                         <div>
                             <Link to="/createAccount">
                                 <button style={{ backgroundColor: "darkred" }} className="btn btn-primary" id="signUpButton" type="button">Create an account</button>
