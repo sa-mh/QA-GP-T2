a13cf9c4e8af438717df7d256e4ad4f215c2435d
Ubuntu <ubuntu@ip-172-31-44-90.eu-west-1.compute.internal>
2020-12-04 14:59:37 +0000

    Ansible roles organised and scripts assigned

---
+++ Infra/Ansible/roles/Docker/tasks/docker-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
