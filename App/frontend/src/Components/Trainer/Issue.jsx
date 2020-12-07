87bbf87a7b63ac44a9ca344fd65ed66e45a0e335
Ramgithj <RJagajith@academytrainee.com>
2020-12-07 14:09:42 +0000

    Implemented the use of props

---
+++ App/frontend/src/Components/Trainer/Issue.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Issue = (props) => {
+
+    return (
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-body" id="issue-topic">Topic: {props.topic}</p>
+                    <p className="card-body">{props.message}</p>
+                    <p className="card-body" id="issue-date">Created on: {props.date}</p>
+                    <p className="card-body" id="issue-priority">Priority: {props.priority}</p>
+                    <button className="card-body" id="solve-button">Solve</button>
+                </div>
+            </div>
+
+
+{/* The code below should be deleted once axios has been set up. This is for demo purpose only */}
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-body" id="issue-topic">Topic: {props.topic}</p>
+                    <p className="card-body">{props.message}</p>
+                    <p className="card-body" id="issue-date">Created on: {props.date}</p>
+                    <p className="card-body" id="issue-priority">Priority: {props.priority}</p>
+                    <button className="card-body" id="solve-button">Solve</button>
+
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-body" id="issue-topic">Topic: {props.topic}</p>
+                    <p className="card-body">{props.message}</p>
+                    <p className="card-body" id="issue-date">Created on: {props.date}</p>
+                    <p className="card-body" id="issue-priority">Priority: {props.priority}</p>
+                    <button className="card-body" id="solve-button">Solve</button>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Issue;
