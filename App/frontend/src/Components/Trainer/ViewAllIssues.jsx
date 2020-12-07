6c9a94deeeccf982b1a50e6961470ab7ec2b360f
Ramgithj <RJagajith@academytrainee.com>
2020-12-07 14:11:21 +0000

    Implemented the use of props

--- App/frontend/src/Components/Trainer/ViewAllIssues.jsx
+++ App/frontend/src/Components/Trainer/ViewAllIssues.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Issue from './Issue';
+
 
 const ViewAllIssues = () => {
 
@@ -7,18 +9,35 @@ const ViewAllIssues = () => {
 
 
     // making a get request which pulls the issues from the database - NOT SURE IF THIS WORKS. please Fix!
-    axios.get("http://localhost:8080/")
+    axios.get("/trainer/getAll")
         .then(response => {
             console.log(response.data);
             setData(response.data);
         });
 
+
+    //The data saved from the API call should be then used to display it on the page. Right now I have just hard coded some text in there to see how the page would look. 
     return (
-        <div>
+        <>
+        <h1> View all the Issues here</h1>
 
-            
-        </div>
-    )
+        <Issue 
+        title={"The title of the issue appears here"} 
+        message={"Get Axios to pull and display data"}
+        topic={"Chosen topic"}
+        date={"01/02/03"}
+        priority={"High"}
+        />
 
-}
+
+        <div class="vl"></div>
+        
+        </>
+    );
+
+};
 export default ViewAllIssues;
+
+// {/* {data.map((issue) => (
+//             <Issue title={issue.title} message={issue.message}/>
+//         ))} */}
