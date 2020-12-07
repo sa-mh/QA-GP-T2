5333a07acda17ca18cc9cf9a1cc5b88f27da287e
SherazShahid017 <71394541+SherazShahid017@users.noreply.github.com>
2020-12-07 15:28:49 +0000

    Pascal casing changed

--- App/Database/Create.sql
+++ App/Database/Create.sql
@@ -2,43 +2,43 @@ CREATE DATABASE IF NOT EXISTS `test`;
 
 USE `test`;
 
-CREATE TABLE IF NOT EXISTS `Trainer` (
-  `trainerId` INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `trainer` (
+  `trainer_id` INTEGER NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(60) NOT NULL,
-  `firstName` VARCHAR(60) NOT NULL,
-  `lastName` VARCHAR(60) NOT NULL,
+  `first_name` VARCHAR(60) NOT NULL,
+  `last_name` VARCHAR(60) NOT NULL,
   `field` VARCHAR(30) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`trainerId`)
+  PRIMARY KEY (`trainer_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Tickets_Trainee` (
-  `ticketId` INT NOT NULL,
-  `traineeId` INT NOT NULL,
-  PRIMARY KEY (`ticketId`, `traineeId`)
+CREATE TABLE IF NOT EXISTS `tickets_trainee` (
+  `ticket_id` INT NOT NULL,
+  `trainee_id` INT NOT NULL,
+  PRIMARY KEY (`ticket_id`, `trainee_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Trainee` (
-  `traineeId` INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `trainee` (
+  `trainee_id` INTEGER NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(60) NOT NULL,
-  `firstName` VARCHAR(60) NOT NULL,
-  `lastName` VARCHAR(60) NOT NULL,
+  `first_name` VARCHAR(60) NOT NULL,
+  `last_name` VARCHAR(60) NOT NULL,
   `cohort` VARCHAR(40) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`traineeId`)
+  PRIMARY KEY (`trainee_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Ticket` (
-  `ticketId` INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ticket` (
+  `ticket_id` INTEGER NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(20) NOT NULL,
   `issue` VARCHAR(244) NOT NULL,
   `topic` VARCHAR(30) NOT NULL,
-  `submitDate` TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `submit_date` TIMESTAMP NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `urgency` INT NOT NULL,
   `status` VARCHAR(20) NOT NULL,
-  `traineeId` INT NOT NULL,
+  `trainee_id` INT NOT NULL,
   `trainerId` INT NOT NULL,
-  PRIMARY KEY (`ticketId`)
+  PRIMARY KEY (`ticket_id`)
 );
 
-INSERT INTO `Trainer` VALUES (1,'BenDover1','Ben', 'Dover', 'DevOps', 'mypass321');
+INSERT INTO `trainer` VALUES (1,'BenDover1','Ben', 'Dover', 'DevOps', 'mypass321');
