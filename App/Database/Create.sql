e21c27e04c25a063e70547b19df9491b9ed58eb7
SherazShahid017 <SShahid2@qa.com>
2020-12-09 12:10:48 +0000

    new values added to check tickets and trainee

--- App/Database/Create.sql
+++ App/Database/Create.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `trainee` (
   `trainee_id` INT NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(60) NOT NULL,
   `first_name` VARCHAR(60) NOT NULL,
-  `last_name` VARCHAR(60) NOT NULL,
+  `second_name` VARCHAR(60) NOT NULL,
   `cohort` VARCHAR(40) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `trainee_email` VARCHAR(60) NOT NULL,
@@ -37,14 +37,14 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `title` VARCHAR(20) NOT NULL,
   `issue` VARCHAR(244) NOT NULL,
   `topic` VARCHAR(30) NOT NULL,
-  `submit_date` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `submit_date` timestamp NOT NULl default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `urgency` INT NOT NULL,
   `status` VARCHAR(20) NOT NULL,
   `trainee_id` int NOT NULL,
   `trainer_id` int NOT NULL,
   PRIMARY KEY (`ticket_id`),
   FOREIGN KEY (`trainee_id`) REFERENCES trainee(trainee_id),
-  FOREIGN KEY (`trainer_id`) REFERENCES trainer(traineer_id)
+  FOREIGN KEY (`trainer_id`) REFERENCES trainer(trainer_id)
 );
 
 INSERT INTO `trainer` (
@@ -66,3 +66,44 @@ VALUES (
   'b.dover@gmail.com'
 );
 
+INSERT INTO `trainee` (
+  `trainee_id`,
+  `username`,
+  `first_name`,
+  `second_name`,
+  `cohort`,
+  `password`,
+  `trainee_email`
+)
+VALUES (
+  NULL,
+  'JHarry4',
+  'Jordan',
+  'Harrison', 
+  'Everything',
+  'SudoSecurePass',
+  'j.harrry@gmail.com'
+);
+
+INSERT INTO `ticket` (
+  `ticket_id`,
+  `title`,
+  `issue`,
+  `topic`,
+  `submit_date`,
+  `urgency`,
+  `status`,
+  `trainee_id`,
+  `trainer_id`
+)
+VALUES (
+  NULL,
+  'Help Me Pls',
+  'I cannot get this ticket to display',
+  'MySQL', 
+  '01-JAN-2003 2:00:00',
+  '4',
+  'new',
+  '1',
+  '2'
+);
