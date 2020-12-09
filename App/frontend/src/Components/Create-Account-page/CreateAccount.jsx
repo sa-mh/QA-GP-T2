213472f2e8dc7e16fb1699d36b3af67ee10ad50a
Ramgithj <RJagajith@academytrainee.com>
2020-12-09 17:07:07 +0000

    end of day

--- App/frontend/src/Components/Create-Account-page/CreateAccount.jsx
+++ App/frontend/src/Components/Create-Account-page/CreateAccount.jsx
@@ -59,22 +59,12 @@ const CreateAccount = (props) => {
             <div className="signupDiv">
                 <h1 className="signupHeading">Create an account</h1>
                 <div>
-<<<<<<< HEAD
                     <form className="ml-3" id="signupForm" onSubmit= {validate}>
-=======
-                    <form className="ml-3" id="signupForm" onSubmit={isPasswordSame}>
->>>>>>> 44a0d823f7762d43bd73c3fa578fcf7b6dc431c9
                         <input className="signupInput" type="text" id="first-name" onChange={(e)=>setFirstname(e.target.value)} placeholder="Enter your first name" required></input> <br></br>
                         <input className="signupInput" type="text" id="last-name" onChange={(e)=>setLastname(e.target.value)} placeholder="Enter your last name" required></input> <br></br>
                         <input className="signupInput" type="text" id="qa_email" onChange={(e)=>setQaEmail(e.target.value)} placeholder="Enter your QA Email address" required></input> <br></br>
                         <input className="signupInput" type="text" id="username" onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your username" required></input> <br></br>
                         <select defaultValue="Software Engineer" className="signupInput" name="cohort" id="cohort" onChange={e=>setCohort(e.target.value)}>
-                            <option value="Dev Ops">Dev Ops</option>
-                            <option value="Cloud Native">Cloud Native</option>
-                            <option value="Software Engineer">Software Engineer</option>
-                        </select>
-                        <select className="signupInput" name="cohort" id="cohort" onChange={e=>setCohort(e.target.value)}>
-                            <option value="" selected disabled hidden>Cohort</option>
                             <option value="Dev Ops">Dev Ops</option>
                             <option value="Cloud Native">Cloud Native</option>
                             <option value="Software Engineer">Software Engineer</option>
