9a445ea6ab81d8329c9dafc0cf466e169bf91c76
lisandrodv27 <lvillarruel@qa.com>
2020-12-15 14:22:43 +0000

    ticketdto

--- App/Backend/src/main/java/dto/TicketDto.java
+++ App/Backend/src/main/java/dto/TicketDto.java
@@ -2,15 +2,6 @@ package dto;
 
 import java.util.Date;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@EqualsAndHashCode
 public class TicketDto {
 
 	private Long id;
@@ -25,16 +16,151 @@ public class TicketDto {
 	
 	public TicketDto(Long id, String title, String issue, String topic, Date submitDate, int urgency, String status, int trainerId, int traineeId) {
 		super();
+		setId(id);
+		setTitle(title);
+		setIssue(issue);
+		setTopic(topic);
+		setSubmitDate(submitDate);
+		setUrgency(urgency);
+		setStatus(status);
+		setTrainerId(trainerId);
+		setTraineeId(traineeId);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getIssue() {
+		return issue;
+	}
+
+	public void setIssue(String issue) {
 		this.issue = issue;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
 		this.topic = topic;
+	}
+
+	public Date getSubmitDate() {
+		return submitDate;
+	}
+
+	public void setSubmitDate(Date submitDate) {
 		this.submitDate = submitDate;
+	}
+
+	public int getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(int urgency) {
 		this.urgency = urgency;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
 		this.status = status;
+	}
+
+	public int getTrainerId() {
+		return trainerId;
+	}
+
+	public void setTrainerId(int trainerId) {
 		this.trainerId = trainerId;
+	}
+
+	public int getTraineeId() {
+		return traineeId;
+	}
+
+	public void setTraineeId(int traineeId) {
 		this.traineeId = traineeId;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((issue == null) ? 0 : issue.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((submitDate == null) ? 0 : submitDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((topic == null) ? 0 : topic.hashCode());
+		result = prime * result + traineeId;
+		result = prime * result + trainerId;
+		result = prime * result + urgency;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TicketDto other = (TicketDto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (issue == null) {
+			if (other.issue != null)
+				return false;
+		} else if (!issue.equals(other.issue))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (submitDate == null) {
+			if (other.submitDate != null)
+				return false;
+		} else if (!submitDate.equals(other.submitDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (topic == null) {
+			if (other.topic != null)
+				return false;
+		} else if (!topic.equals(other.topic))
+			return false;
+		if (traineeId != other.traineeId)
+			return false;
+		if (trainerId != other.trainerId)
+			return false;
+		if (urgency != other.urgency)
+			return false;
+		return true;
+	}
 	
 }
