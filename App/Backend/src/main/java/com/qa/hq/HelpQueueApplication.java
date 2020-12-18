5e3626420d2a7a1fb64c1f4b7631759e04e653b2
Lewis Thomson <lewisfalconer@outlook.com>
2020-12-18 10:38:57 +0000

    Some backend properties changes. -LT

--- App/Backend/src/main/java/com/qa/hq/HelpQueueApplication.java
+++ App/Backend/src/main/java/com/qa/hq/HelpQueueApplication.java
@@ -2,6 +2,7 @@ package com.qa.hq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class HelpQueueApplication {
